15dbd92ce8401fb495fa89f0bb73c56ddb276213
Luis Herrera <herrera.lrh@gmail.com>
2020-03-28 17:53:03 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -30,4 +30,11 @@ class Owner
     pets[:fishes] << Fish.new(name)
   end
 
-end
+  def buy_cat(name)
+    pets[:cats] << Cat.new(name)
+  end
+
+  def buy_dog(name)
+    pets[:dogs] << Dog.new(name)
+  end
+  
