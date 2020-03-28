c692558c06824d652bcaf55bbb57567b108a6a05
Luis Herrera <herrera.lrh@gmail.com>
2020-03-28 17:58:12 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -41,4 +41,16 @@ class Owner
   def walk_dogs
     pets[:dogs].each {|dog| dog.mood = 'happy'}
   end
+
+  def play_with_cats
+    pets[:cats].each {|cat| cat.mood = 'happy'}
+  end
+
+  def feed_fish
+    pets[:fishes].each {|fish| fish.mood = 'happy'}
+  end
+
+  def sell_pets
+    pets.each {|pet, s| s.each {|pet| pet.mood = 'nervous'}}
+  end
 end
